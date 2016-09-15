9d0559c38f38c5f50e52fa1ccf13d802517dd93c
jjhlzn <jinjunhang@hotmail.com>
2016-09-15 22:20:35 +0800

    update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,11 @@ require 'date'
 
 module ApplicationHelper
   def get_log_table_name (application, date)
-    return _get_table_name('logsystem_ordersystem', application, date)
+    return _get_table_name('logsystem_jufang', application, date)
   end
 
   def get_request_table_name (application, date)
-    return _get_table_name('logsystem_requests_ordersystem', application, date)
+    return _get_table_name('logsystem_requests_jufang', application, date)
   end
 
   def _get_table_name (table_name, application, date)
